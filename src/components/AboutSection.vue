09e5af805509345ec6267c8208e48879b7496506
Geo-sudo <george.nady.contact@gmail.com>
2024-12-04 19:50:09 +0200

    Num Change

--- src/components/AboutSection.vue
+++ src/components/AboutSection.vue
@@ -3,7 +3,7 @@
     <div>
       <p class="display-4 fw-bold">I'm a senior STEMer</p>
       <p class="fs-3">
-        at STEM High School for Boys - 6<sup>th</sup> of October class of '23
+        at STEM High School for Boys - 6<sup>th</sup> of October class of '25
       </p>
     </div>
   </section>
