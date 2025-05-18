e210e8658d98ca3288c6fd00765b140a3fcbfc94
Geo-sudo <george.nady.contact@gmail.com>
2025-05-18 18:03:26 +0300

    About edits.

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -10,12 +10,7 @@
             student at <span class="keyword">Dartmouth</span>. I have an
             enthusiatic passion for
             <span class="keyword">computer science</span>. I found it at the age
-            of 13, and I will follow it till I RIP. So far, I've been working on
-            my own to grow this passion, and I'm willing to continue this work
-            while studying CS at
-            <!-- <span class="DCollege"> -->
-            dream college.
-            <!-- </span> -->
+            of 13, and I will follow it till I RIP. So far, I've been self-studying to grow this passion (and hosting hackathons with the Hack Foundation to help high schoolers do the same). I have also been mesmerized by Astrophysics/Astornomy and have done a few computational projects.
           </p>
           <svg
             class="spider-web"
